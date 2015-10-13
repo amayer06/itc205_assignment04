c0695fee84f9b063d4fa2688c937d77b157783aa
amayer06 <katbat_@hotmail.com>
2015-10-13 14:14:39 +1100

    Test02 completed and screenshots taken.

--- CrownAndAnchorGame/src/testBug02.java
+++ CrownAndAnchorGame/src/testBug02.java
@@ -7,14 +7,38 @@ import static org.mockito.Mockito.*;
 
 public class testBug02 {
 	
+	private static Player player;
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	
+		player = new Player("Ame", 10);
+	    player.setLimit(0);
 	}
 	
-	@Test
-	public void testSetLimit() {
 	
+	
+	@Test
+	public void testBalanceComparedToLimit() {
+		
+	    int bet = 5;
+
+	    player = new Player("Player01", 10);
+	    System.out.println("Player Balance: " + player.getBalance());
+	    System.out.println("Bet: " + bet);
+	    System.out.println("Does Balance Exceeds Limit? " + player.balanceExceedsLimitBy(bet));
+	    System.out.println();
+
+	    player = new Player("Player02", 5);
+	    System.out.println("Player Balance: " + player.getBalance());
+	    System.out.println("Bet: " + bet);
+	    System.out.println("Does Balance Exceeds Limit? " + player.balanceExceedsLimitBy(bet));
+	    System.out.println();
+	    
+	    player = new Player("Player03", 0);
+	    System.out.println("Player Balance: " + player.getBalance());
+	    System.out.println("Bet: " + bet);
+	    System.out.println("Does Balance Exceeds Limit? " + player.balanceExceedsLimitBy(bet));
 	}
 
 }
